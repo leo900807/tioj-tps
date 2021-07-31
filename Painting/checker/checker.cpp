f5ef866aa8f2ccc65ddf0e4b5f48641b58f32e84
Leo Chen <leo900807@yahoo.com.tw>
2021-07-31 16:10:46 +0800

    Modify checker compile error

--- Painting/checker/checker.cpp
+++ Painting/checker/checker.cpp
@@ -11,7 +11,7 @@ void wa() {
 }
 
 int main(int argc, char *argv[]) {
-	registerChecker("Painting", argc, argv);
+	registerTestlibCmd(argc, argv);
     int n, m, cnt = 0;
     n = inf.readInt();
     m = inf.readInt();
